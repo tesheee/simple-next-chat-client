0c70d2dfd35f8bf4dcdf105286d4fd25409cf26f
Vadim Tonkikh <70746364+tesheee@users.noreply.github.com>
2023-07-18 13:28:44 +0300

    Update root_layout.tsx

--- pages/root_layout.tsx
+++ pages/root_layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/header";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: any) {
   return (
     <>
       <Header></Header>
